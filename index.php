7d38ce39cc9d3a589648cfdde16dd988bc822f4e
arcama <arcama2122daw2@protonmail.com>
2022-03-14 20:48:37 +0100

    prmimer commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+	<head>
+		<title>
+			PÀGINA WEB INICIAL DE L'APLICACIÓ
+		</title>
+	</head>
+	<body>
+		<h1> APLICACIÓ D'ACCÉS A BASES DE DADES LDAP</h1>
+		<h2> DAW2 M08UF2 M08UF3 </h2>
+		<h3> Autor: arcama2122daw2</h3>
+		<h3> Correu: arcama2122daw2@protonmail.com</h3>
+		<h3> Github: https://github.com/arcama2122daw2/daw2_m08uf23_projecte_carrasco_arnau.git</h3>
+		<a href="http://zend-arcama.fjeclot.net/autent/login.php">Inicia sessió</a>
+	</body>
+</html>
