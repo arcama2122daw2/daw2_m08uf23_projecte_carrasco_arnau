ffee0b0e7485ab2e7da3947da7c68bb113617523
arcama <arcama2122daw2@protonmail.com>
2022-03-21 20:55:12 +0100

    Eliminar, modificar y crear usuarios

--- login.php
+++ login.php
@@ -1,24 +1,3 @@
-<?php
-	require 'vendor/autoload.php';
-	use Laminas\Ldap\Ldap;
-	$domini = 'dc=fjeclot,dc=net';
-	$opcions = [
-		'host' => 'zend-arcama.fjeclot.net',
-		'username' => "cn=admin,$domini",
-		'password' => 'fjeclot',
-		'bindRequiresDn' => true,
-		'accountDomainName' => 'fjeclot.net',
-		'baseDn' => 'dc=fjeclot,dc=net',
-	];
-	$ldap = new Ldap($opcions);
-	$ldap->bind();
-	$usuari=$ldap->getEntry('uid=sysdev,ou=desenvolupadors,dc=fjeclot,dc=net');
-	echo "<b><u>".$usuari["dn"]."</b></u><br>";
-	foreach ($usuari as $atribut => $dada) {
-		if ($atribut != "dn") echo $atribut.": ".$dada[0].'<br>';
-	}
-?>
-
 <html>
 	<head>
 		<title>
--- menu.php
+++ menu.php
@@ -7,6 +7,9 @@
 	<body>
 		<h2> MENÚ PRINCIPAL DELPROJECTE DE BASES DE DADES LDAP</h2>
 		<h3> <b>En construcció!!!!!!!!!!!</b> </h3>
+		<a href="http://zend-arcama.fjeclot.net/projecte_ldap/crearU.php">Crear usuari</a>
+		<a href="http://zend-arcama.fjeclot.net/projecte_ldap/modificarU.php">Modificar usuari</a>
+		<a href="http://zend-arcama.fjeclot.net/projecte_ldap/eliminarU.php">Eliminar usuari</a>
 		<a href="http://zend-arcama.fjeclot.net/projecte_ldap/index.php">Torna a la pàgina inicial</a>
 	</body>
 </html>
