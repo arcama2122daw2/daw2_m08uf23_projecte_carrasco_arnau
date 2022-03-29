c3506a4100d9892cc290d90c6f301c9a5b3240d0
arcama <arcama2122daw2@protonmail.com>
2022-03-29 15:49:50 +0200

    Arreglat eliminarU

--- elimianarU.php
+++ elimianarU.php
@@ -1,34 +1,51 @@
 <?php
-	require 'vendor/autoload.php';
-	use Laminas\Ldap\Attribute;
-	use Laminas\Ldap\Ldap;
-	
-	ini_set('display_errors', 0);
-	#
-	# Entrada a esborrar: usuari 3 creat amb el projecte zendldap2
-	#
-	$uid = 'usr3';
-	$unorg = 'usuaris';
-	$dn = 'uid='.$uid.',ou='.$unorg.',dc=fjeclot,dc=net';
-	#
-	#Opcions de la connexió al servidor i base de dades LDAP
-	$opcions = [
-		'host' => 'zend-arcama.fjeclot.net',
-		'username' => 'cn=admin,dc=fjeclot,dc=net',
-		'password' => 'fjeclot',
-		'bindRequiresDn' => true,
-		'accountDomainName' => 'fjeclot.net',
-		'baseDn' => 'dc=fjeclot,dc=net',		
-	];
-	#
-	# Esborrant l'entrada
-	#
-	$ldap = new Ldap($opcions);
-	$ldap->bind();
-	try{
-	    $ldap->delete($uid);
-	    echo "<b>Entrada esborrada</b><br>"; 
-	} catch (Exception $e){
-	   echo "<b>Aquesta entrada no existeix</b><br>";
-	}
+require 'vendor/autoload.php';
+use Laminas\Ldap\Ldap;
+
+ini_set('display_errors', 0);
+
+if ($_POST['method'] == "DELETE") {
+    if ($_POST['uid'] && $_POST['unitat_organitzativa']){
+        $uid = $_POST['uid'];
+        $unitat_organitzativa = $_POST['unitat_organitzativa'];
+        $dn = 'uid='.$uid.',unitat_organitzativa='.$unitat_organitzativa.',dc=fjeclot,dc=net';
+        
+        $opcions = [
+            'host' => 'zend-arcama.fjeclot.net',
+            'username' => "cn=admin,dc=fjeclot,dc=net",
+            'password' => 'fjeclot',
+            'bindRequiresDn' => true,
+            'accountDomainName' => 'fjeclot.net',
+            'baseDn' => 'dc=fjeclot,dc=net',
+        ];
+        
+        $ldap = new Ldap($opcions);
+        $ldap->bind();
+        $Esborrat = false;
+        try{
+            if ($ldap->delete($dn)) echo "<b>Esborrat correctament</b><br>";
+        } catch (Exception $e){
+            echo "<b>Text incorrect</b><br><br>";
+        }
+    }
+}
+
 ?>
+<html>
+	<head>
+		<title>
+			ESBORRANT USUARI
+		</title>
+	</head>
+	<body>
+	<h1>ESBORRANT USUARI</h1>
+		<form action="http://zend-arcama.fjeclot.net/projecte_ldap/eliminarU.php" method="POST">
+			<input type="text" name="method" value="DELETE" class="amaga"><br><br>
+			UID: <input type="text" name="uid"><br>
+			Unitat organitzativa: <input type="text" name="unitat_organitzativa"><br>
+			<input type="submit" value="Envia" />
+			<input type="reset" value="Neteja" />
+		</form>
+		<a href="http://zend-arcama.fjeclot.net/projecte_ldap/menu.php">Torna al menu</a>
+	</body>
+</html>
